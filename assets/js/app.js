047ddbb5fd93877fb475d3e19c52275229276295
Jason Harwig <jason.harwig@nearinfinity.com>
2012-05-07 11:51:09 -0500

    support wider design by adding more padding to sides

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,3 +4,20 @@ function shareLink(link, e) {
     if (e.preventDefault) e.preventDefault();
     return false;
 }
+
+if (window.addEventListener) {
+    window.addEventListener('keyup', function(e) {
+        if (e.keyCode == 71) {
+            var id = 'gridOverlay';
+            var div = document.getElementById(id)
+            if (div) {
+                div.parentNode.removeChild(div);
+            } else {
+                div = document.createElement('div');
+                div.id = id;
+                div.className = id;
+                document.body.appendChild(div);
+            }
+        }
+    }, false);
+}
