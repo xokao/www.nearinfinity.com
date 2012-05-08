015903f8d6d1da2d85ae053f5af804a313ca13c7
Jason Harwig <jason.harwig@nearinfinity.com>
2012-05-08 14:27:13 -0500

    add js libs

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,4 @@
+
 function shareLink(link, e) {
     window.open(link.href, 'sharewindow','toolbar=0,status=0,width=626,height=436');
     if (e.stopPropagation) e.stopPropagation();
@@ -5,35 +6,31 @@ function shareLink(link, e) {
     return false;
 }
 
-if (window.addEventListener) {
-    window.addEventListener('keyup', function(e) {
+$(function() {
+    $(window).on('keyup', function(e) {
         if (e.keyCode == 71) {
-            var id = 'gridOverlay';
-            var div = document.getElementById(id)
-            if (div) {
-                div.parentNode.removeChild(div);
+            var cls = 'gridOverlay';
+            var div = $('.'+cls)
+            if (div.length) {
+                div.remove()
             } else {
-                div = document.createElement('div');
-                div.id = id;
-                div.className = id;
-                document.body.appendChild(div);
+                $(document.body).append('<div class="'+cls+'"></div>');
             }
         }
-    }, false);
+    });
 
-    window.addEventListener('DOMContentLoaded', function() {
-        var images = [
-            'big-data-trans',
-            'mobile',
-            'trusted-apps',
-            'join-us'
-        ];
-        var index = 1;
-        document.getElementById('main').addEventListener('click', function() {
-            var div = this.getElementsByTagName('figure')[0].childNodes[0]
-            div.style.background = 'url(assets/images/impact/' + images[(++index % images.length)] + '.png) 15px 0px no-repeat'
-        }, false);
-    }, false);
-}
-
+    /*
+    // TEMP Header image rotation
+    var classes = [
+        'bigdata',
+        'mobile',
+        'trustedapps',
+        'joinus'
+    ];
+    var index = 0;
+    $('#main').on('click', function() {
+        $(this).removeClass(classes.join(' ')).addClass(classes[++index % classes.length]);
+    });
+    */
+});
 
