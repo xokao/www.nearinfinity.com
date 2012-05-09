c6313ad2d91debc3dbf586482c340fc95f1f8d3d
Brandon Marc-Aurele <bmarcaur@nearinfinity.com>
2012-05-09 17:45:59 -0400

    more work with generators and reuse

--- _plugins/assets.rb
+++ _plugins/assets.rb
@@ -15,17 +15,15 @@ module Jekyll
       "#{Tagger::TAG_PAGE_DIR + ERB::Util.u(tag)}#{'.html'}"
     end
 
+    # Returns a comma seperated list off all tags for an object
     def tags(obj)
       tags = obj['tags'][0].is_a?(Array) ? obj['tags'].map{ |t| t[0] } : obj['tags']
-      tags.map { |t| tag_link(t, tag_url(t)) if t.is_a?(String) }.compact.join(', ')
+      tags.map { |t| tag_link(t.downcase, tag_url(t)) if t.is_a?(String) }.compact.join(', ')
     end
 
-    def all_tags_list(site)
-      html = ''
-      site['tags'].each do |tag_key, tag_value|
-        html << "<li><a href='#{tag_url(tag_key.downcase)}'>#{tag_key.downcase}</a></li>"
-      end
-      html
+    # Returns a list off all blog tags
+    def tags_list(obj)
+      obj['tags'].map { |tag_key, tag_value| "<li>#{tag_link(tag_key.downcase, tag_url(tag_key))}</li>" }.compact.join
     end
   end
 end
