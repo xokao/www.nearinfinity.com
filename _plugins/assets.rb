b1c0f6a9e246e84e7e113c72f2d3a9e44fe6ed47
Brandon Marc-Aurele <bmarcaur@nearinfinity.com>
2012-05-10 11:56:59 -0400

    more filters and cleanup

--- _plugins/assets.rb
+++ _plugins/assets.rb
@@ -1,29 +1,20 @@
 module Jekyll
   module AssetFilters
+    # Returns a users image url
     def user_image(input)  
       "/assets/images/users/#{input}.png"
     end
 
-    def tag_link(tag, url = tag_url(tag), html_opts = nil)
-      unless html_opts.nil?
-        html_opts = ' ' + html_opts.map { |k, v| %Q{#{k}="#{v}"} }.join(' ')
-      end
-      %Q{<a href="#{url}"#{html_opts}>#{tag}</a>}
+    # Returns a list of all authors
+    def authors_list(site, sort = true)
+      authors = site['categories'].keys.reject {|category|
+        ['blogs', 'techtalks', 'news', 'speaking'].include? category
+      }.sort
+      authors.map { |author| "<li><a href='#{author_url(author)}' class='author-filter'>#{author.gsub('_', ' ')}</a></li>" }.compact.join
     end
 
-    def tag_url(tag)
-      "#{Tagger::TAG_PAGE_DIR + ERB::Util.u(tag)}#{'.html'}"
-    end
-
-    # Returns a comma seperated list off all tags for an object
-    def tags(obj)
-      tags = obj['tags'][0].is_a?(Array) ? obj['tags'].map{ |t| t[0] } : obj['tags']
-      tags.map { |t| tag_link(t.downcase, tag_url(t)) if t.is_a?(String) }.compact.join(', ')
-    end
-
-    # Returns a list off all blog tags
-    def tags_list(obj)
-      obj['tags'].map { |tag_key, tag_value| "<li>#{tag_link(tag_key.downcase, tag_url(tag_key))}</li>" }.compact.join
+    def author_url(author)
+      "/blogs/#{author}"
     end
   end
 end
---
+++ _plugins/tag_filters.rb
@@ -0,0 +1,29 @@
+module Jekyll
+  module TagFilters
+    # Create an href for a tag
+    def tag_link(tag, url = tag_url(tag), html_opts = nil)
+      unless html_opts.nil?
+        html_opts = ' ' + html_opts.map { |k, v| %Q{#{k}="#{v}"} }.join(' ')
+      end
+      %Q{<a href="#{url}"#{html_opts}>#{tag}</a>}
+    end
+
+    # Generates the url for a specifc tag
+    def tag_url(tag)
+      "#{Tagger::TAG_PAGE_DIR + ERB::Util.u(tag)}#{'.html'}"
+    end
+
+    # Returns a comma seperated list off all tags for an object
+    def tags(obj)
+      tags = obj['tags'][0].is_a?(Array) ? obj['tags'].map{ |t| t[0] } : obj['tags']
+      tags.sort.map { |t| tag_link(t.downcase, tag_url(t)) if t.is_a?(String) }.compact.join(', ')
+    end
+
+    # Returns a list off all blog tags
+    def tags_list(obj)
+      obj['tags'].sort.map { |tag_key, tag_value| "<li>#{tag_link(tag_key.downcase, tag_url(tag_key))}</li>" }.compact.join
+    end
+  end
+end
+
+Liquid::Template.register_filter(Jekyll::TagFilters)
