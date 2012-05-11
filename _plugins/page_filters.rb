f7b784d4acb1ef09a4850e8c4a2a712d22442005
Matt Rohr <mrohr@nearinfinity.com>
2012-05-11 11:46:09 -0400

    added offset to homepage post rendering, fixed moreblogs on homepage

--- _plugins/page_filters.rb
+++ _plugins/page_filters.rb
@@ -1,26 +1,26 @@
 module Jekyll
   module BlogsFilters
-    def render_homepage_posts(amount,type,template)
+    def render_homepage_posts(amount,type,template,offset)
       all_blogs = @context.registers[:site].categories[type]
       hiddenIds = @context.registers[:site].config['hideFromHomepage']
       filtered_blogs = all_blogs.reject{ |blog| hiddenIds.include? blog.id }
-      filtered_blogs.sort{|a,b| b.date <=> a.date}[0..(amount - 1)]
+      filtered_blogs.sort{|a,b| b.date <=> a.date}[offset..((offset + amount) - 1)]
         .map{|blog| include_template(template, {'post' => blog})}.compact.join
     end
 
     # Render the short blog partial for the home page
-    def render_homepage_blogs(amount)
-      render_homepage_posts(amount,'blogs','homepage/blog.html')
+    def render_homepage_blogs(amount, offset=0)
+      render_homepage_posts(amount,'blogs','homepage/blog.html',offset)
     end
 
     # Render the short techtalks partial for the home page
-    def render_homepage_techtalks(amount)
-      render_homepage_posts(amount,'techtalks','homepage/techtalk.html')
+    def render_homepage_techtalks(amount,offset=0)
+      render_homepage_posts(amount,'techtalks','homepage/techtalk.html',offset)
     end
 
     # Render the short speaking partial for the home page
-    def render_homepage_speaking(amount)
-      render_homepage_posts(amount,'speaking','homepage/speaking.html')
+    def render_homepage_speaking(amount,offset=0)
+      render_homepage_posts(amount,'speaking','homepage/speaking.html',offset)
     end
   end
 end
