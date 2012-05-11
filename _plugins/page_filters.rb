4015a657b6094f60ae4a7be6205da866bc28a2a9
Brandon Marc-Aurele <bmarcaur@nearinfinity.com>
2012-05-11 14:29:18 -0400

    similar tag blog lookup

--- _plugins/page_filters.rb
+++ _plugins/page_filters.rb
@@ -22,6 +22,39 @@ module Jekyll
     def render_homepage_speaking(amount)
       render_homepage_posts(amount,'speaking','homepage/speaking.html')
     end
+
+    def blog_with_similar_tags(blog, count = 5)
+      return if blog['tags'].count == 0
+      similarity_index = create_similarity_index blog, @context.registers[:site].categories['blogs']
+      limited_related_blogs = create_most_related_array similarity_index
+      limited_related_blogs[0..count].map{ |post|
+        "<li>#{include_template('post-list-item.html', {'post' => post})}</li>"
+      }.compact.join
+    end
+
+    private
+    def create_similarity_index(blog, all_blogs)
+      similarity_index = {}
+      all_blogs.each do |compared_blog|
+        next if compared_blog.id == blog['id']
+        similarity = (blog['tags'] & compared_blog.tags).length
+        similarity_index[similarity] = [] unless similarity_index.has_key? similarity
+        similarity_index[similarity] << compared_blog
+      end
+      # Remove the unrelated posts (efficiency)
+      similarity_index.delete 0
+      similarity_index.each{|key, blog_list| blog_list.sort!{|a,b| b.date <=> a.date}}
+    end
+
+    def create_most_related_array(similarity_index)
+      limited_related_blogs = []
+      similarity_index.keys.reverse.each do |index|
+        break if limited_related_blogs.count >= 5
+        limited_related_blogs << similarity_index[index]
+        limited_related_blogs.flatten!
+      end
+      limited_related_blogs
+    end
   end
 end
 
