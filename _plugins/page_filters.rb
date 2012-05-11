e05e7852a20e237031f2e20bda335a8f16b293b5
Brandon Marc-Aurele <bmarcaur@nearinfinity.com>
2012-05-11 15:47:10 -0400

    formatting and comments on our filters

--- _plugins/page_filters.rb
+++ _plugins/page_filters.rb
@@ -1,14 +1,7 @@
 module Jekyll
   module BlogsFilters
-    def render_homepage_posts(amount,type,template,offset)
-      all_blogs = @context.registers[:site].categories[type]
-      hiddenIds = @context.registers[:site].config['hideFromHomepage']
-      filtered_blogs = all_blogs.reject{ |blog| hiddenIds.include? blog.id }
-      filtered_blogs.sort{|a,b| b.date <=> a.date}[offset..((offset + amount) - 1)]
-        .map{|blog| include_template(template, {'post' => blog})}.compact.join
-    end
-
-    def n_posts_from_date(posts,amount,date)
+    # Returns a specific number of posts that have happened since a certain date
+    def n_posts_from_date(posts, amount,date)
       posts.sort!{|a,b|a.date <=> b.date}
       filtered_posts = Array.new
       previous_size = -1
@@ -30,21 +23,23 @@ module Jekyll
     end
 
     # Render the short techtalks partial for the home page
-    def render_homepage_techtalks(amount,offset=0)
+    def render_homepage_techtalks(amount, offset=0)
       render_homepage_posts(amount,'techtalks','homepage/techtalk.html',offset)
     end
 
     # Render the short speaking partial for the home page
-    def render_homepage_speaking(amount,offset=0)
+    def render_homepage_speaking(amount, offset=0)
       render_homepage_posts(amount,'speaking','homepage/speaking.html',offset)
     end
 
-    def render_future_speaking(template,amount)
+    # Shows the list of future speaking engagements
+    def render_future_speaking(template, amount)
       all_speaking = @context.registers[:site].categories['speaking']
       future_speaking = n_posts_from_date(all_speaking,amount,Time.new)
       future_speaking.map{|post| include_template(template, {'post' => post})}.compact.join
     end
 
+    # Renders the recent news items
     def render_recent_news(template,amount,days_expire)
       all_news = @context.registers[:site].categories['news']
       time = Time.new - (days_expire * (60 * 60 * 24))
@@ -52,6 +47,7 @@ module Jekyll
       recent_news.map{|post| include_template(template, {'news' => post})}.compact.join
     end
 
+    # Renders the count amount of related blogs
     def blog_with_similar_tags(blog, count = 5)
       return if blog['tags'].count == 0
       similarity_index = create_similarity_index blog, @context.registers[:site].categories['blogs']
@@ -62,10 +58,21 @@ module Jekyll
     end
 
     private
+    # Renders the list of type posts for the home page
+    def render_homepage_posts(amount, type, template,offset)
+      all_blogs = @context.registers[:site].categories[type]
+      hiddenIds = @context.registers[:site].config['hideFromHomepage']
+      filtered_blogs = all_blogs.reject{ |blog| hiddenIds.include? blog.id }
+      filtered_blogs.sort{|a,b| b.date <=> a.date}[offset..((offset + amount) - 1)]
+        .map{|blog| include_template(template, {'post' => blog})}.compact.join
+    end
+
+    # Creates a hash of key: similarity number to value: array of posts with that value
     def create_similarity_index(blog, all_blogs)
       similarity_index = {}
       all_blogs.each do |compared_blog|
         next if compared_blog.id == blog['id']
+        # Number of same tags (similarity number)
         similarity = (blog['tags'] & compared_blog.tags).length
         similarity_index[similarity] = [] unless similarity_index.has_key? similarity
         similarity_index[similarity] << compared_blog
@@ -75,6 +82,7 @@ module Jekyll
       similarity_index.each{|key, blog_list| blog_list.sort!{|a,b| b.date <=> a.date}}
     end
 
+    # Pulls from a similarity hash and creates an array of the most relevant posts
     def create_most_related_array(similarity_index)
       limited_related_blogs = []
       similarity_index.keys.reverse.each do |index|
