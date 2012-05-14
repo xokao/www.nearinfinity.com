3a8c1bba521398092f401fb944958c9581f4f0a3
Matt Rohr <mrohr@nearinfinity.com>
2012-05-14 10:48:01 -0400

    fixed tag_list and tags filter links

--- _plugins/tag_filters.rb
+++ _plugins/tag_filters.rb
@@ -2,14 +2,14 @@ module Jekyll
   module TagFilters
     # Returns a comma seperated list off all tags for a post
     def tags(post, page)
-      type = page['type'] || page.categories[0]
+      type =page['type'] || page['url'].split('/')[1]
       tags = post['tags'][0].is_a?(Array) ? post['tags'].map{ |t| t[0] } : post['tags']
       tags.sort.map { |t| "<a href='/tags/#{type}/#{t}.html'>#{t}</a>" if t.is_a?(String) }.compact.join(', ')
     end
 
     # Returns a list off all blog tags
     def tags_list(site, page)
-      type = page['type'] || page.categories[0]
+      type =page['type'] || page['url'].split('/')[1]
       site['tags'].sort.map { |tag_key, tag_value| "<li><a href='/tags/#{type}/#{tag_key}.html'>#{tag_key}</a></li>" }.compact.join
     end
   end
