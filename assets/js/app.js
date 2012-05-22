ecf00d7e4b5de077aa29a4c8ce8a5c5c87fd6bcb
Jason Harwig <jason.harwig@nearinfinity.com>
2012-05-22 12:03:27 -0500

    new popup

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,19 +7,44 @@ function shareLink(link, e) {
 }
 
 $(function() {
+    console.log('b');
+
+    
     // USER POPUP
     var userImageRegex = /assets\/images\/users\/(.*)\.png/;
     var userPopupTimer;
     $(document.body).on(
         {
             'mouseover': function(e) {
+
                 var img = e.target;
                 var match = img && img.src && img.src.match(userImageRegex);
                 var user = match && match.length == 2 && match[1];
 
                 clearTimeout(userPopupTimer);
-                if (user) {
+                if (user && $(img).parents('.bio-popup').length === 0) {
+                    // Start loading html
+                    var doneLoading = false, doneWaiting = false;
+                    var addToPage = function() {
+                        cap.append($(img).clone());
+                        var offset = $(img).offset()
+                        delete offset.height;
+                        offset.width = Math.max(275, offset.width);
+                        offset.left -= 12;
+                        offset.top -= 12;
+                        console.log(offset);
+                        $(document.body).append(cap.css(offset));
+                    };
+                    var cap = $('<div class="bio-popup"/>').load('/short_bios/popup_bio/'+user+'.html', function() {
+                        doneLoading = true;
+                        if (doneWaiting) addToPage();
+                    });
+
                     userPopupTimer = setTimeout(function() {
+                        doneWaiting = true;
+                        if (doneLoading) addToPage();
+
+                        /*
                         var figure = $(img).parent('figure');
                         if (figure.length) {
                             figure.find('figcaption').show();
@@ -34,15 +59,19 @@ $(function() {
                                 }, 10);
                             });
                         }
+                        */
                     }, 250);
                 }   
             },
             'mouseout': function(e) {
                 clearTimeout(userPopupTimer);
+                $('.bio-popup').remove()
+                /*
                 $('figure.user_popup.show_bio figcaption').on('webkitTransitionEnd', function() {
                     $(this).hide().off('webkitTransitionEnd');
                 })
                 $('figure.user_popup.show_bio').removeClass('show_bio');
+                */
             } 
         },
         'img'
