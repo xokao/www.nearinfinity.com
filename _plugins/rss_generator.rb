658cb1221c08c17564793e81164ac4e922957d68
Brandon Marc-Aurele <bmarcaur@nearinfinity.com>
2012-06-05 18:41:27 -0400

    beginning of rss feeds

---
+++ _plugins/rss_generator.rb
@@ -0,0 +1,57 @@
+module Jekyll
+  class RssFeed < Page
+    def initialize(site, base, dir, name, data)
+      self.data = data.clone
+      self.data['layout'] = 'rss_feed'
+      super(site, base, dir, name)
+    end
+
+    def read_yaml(_, __)
+      # Do nothing, Allows you to make pages that dont have pages at the location they are built in
+    end
+  end
+
+  class RssFeedGenerator < Generator
+    safe true
+
+    def generate(site)
+      allblogs = site.posts.reject{|post| post.categories[0] != 'blogs' }
+      sorted_blogs = {}
+
+      allblogs.each do |post|
+        selected_user = sorted_blogs[post.categories[1]] ||= []
+        selected_user << post
+      end
+
+      site.pages << RssFeed.new(site, site.source, '/blogs/rss/', 'index.xml', {
+        'posts'  => allblogs,
+        'title' => "Blogs at Near Infinity",
+        'link' => "http://www.nearinfinity.com/blogs",
+        'description' => 'Employee Blogs'
+      })
+
+      # Generate the Authors Feeds
+      sorted_blogs.each do |author, author_posts|
+        formatted_author = author.split('_').collect{|x| x.capitalize}.join(' ')
+        site.pages << RssFeed.new(site, site.source, "/blogs/rss/#{author}", 'index.xml', {
+          'posts'  => author_posts,
+          'title' => "#{formatted_author} - Blogs at Near Infinity",
+          'link' => "http://www.nearinfinity.com/blogs/#{author}",
+          'description' => "#{formatted_author}'s Blogs"
+        })
+      end
+
+      # Generate the Tags Feeds
+      site.tags.each do |tag, tag_posts|
+        tag = tag.downcase
+        filtered_posts = tag_posts.reject{ |post| !post.categories.include? 'blogs' }.sort.reverse
+        site.pages << RssFeed.new(site, site.source, "/blogs/rss/#{tag}", 'index.xml', {
+          'posts'  => filtered_posts,
+          'title' => "#{tag.upcase} Related Blogs - Blogs at Near Infinity",
+          'link' => "http://www.nearinfinity.com/tags/blogs/#{tag}",
+          'description' => "#{tag} Blogs"
+        })
+      end
+    end
+  end
+end
