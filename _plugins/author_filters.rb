ff3daf6f1a133bf51721498312c33f46c5bc5832
Brandon Marc-Aurele <bmarcaur@nearinfinity.com>
2012-06-06 14:22:12 -0400

    copy paste mistake

--- _plugins/author_filters.rb
+++ _plugins/author_filters.rb
@@ -10,8 +10,8 @@ module Jekyll
     end
 
     def author_url(user)
-      "/who_we_are/bios/#{name}" if Dir.exists?("/who_we_are/bios/#{name}")
-      "/blogs/#{name}"
+      "/who_we_are/bios/#{user}" if Dir.exists?("/who_we_are/bios/#{user}")
+      "/blogs/#{user}"
     end
 
     # Return a list of the COUNT most recent blogs by an author
