092e5ee26b49be3b836f48de9e425e7ee89327b2
Brandon Marc-Aurele <bmarcaur@nearinfinity.com>
2012-06-11 14:08:26 -0400

    tech talk per author generator, removed old data from manageement bios

--- _plugins/talk_data_generator.rb
+++ _plugins/talk_data_generator.rb
@@ -1,8 +1,8 @@
 module Jekyll
   class PagedTalk < Page
-    def initialize(site, base, dir, name, data)
+    def initialize(site, base, dir, name, layout, data)
       self.data = data.clone
-      self.data['layout'] = 'alltalks'
+      self.data['layout'] = layout
       self.data['type'] = 'techtalks'
       super(site, base, dir, name)
     end
@@ -20,16 +20,24 @@ module Jekyll
       alltalks.each do |post|
         post.data['layout'] = 'techtalks'
       end
+      user_talks = alltalks.clone
+      create_all_techtalks(site, alltalks)
+      create_all_user_techtalks(site, user_talks)
+    end
 
+    private
+
+    def create_all_techtalks(site, alltalks)
+      layout = 'alltalks'
       total = (alltalks.length / 15.to_f).ceil
       first_page_posts = alltalks.shift(15)
-      site.pages << PagedTalk.new(site, site.source, '/techtalks/', 'index.html', {
+      site.pages << PagedTalk.new(site, site.source, '/techtalks/', 'index.html', layout, {
         'posts'  => first_page_posts,
         'current_page' => 1,
         'num_pages' => total,
         'title' => 'Recent Tech Talks'
       })
-      site.pages << PagedTalk.new(site, site.source, '/techtalks/page_1/', 'index.html', {
+      site.pages << PagedTalk.new(site, site.source, '/techtalks/page_1/', 'index.html', layout, {
         'posts'  => first_page_posts,
         'current_page' => 1,
         'num_pages' => total,
@@ -37,7 +45,7 @@ module Jekyll
       })
       current_page = 2
       while alltalks.size > 0
-        site.pages << PagedTalk.new(site, site.source, "/techtalks/page_#{current_page}/", 'index.html', {
+        site.pages << PagedTalk.new(site, site.source, "/techtalks/page_#{current_page}/", 'index.html', layout, {
           'posts'  => alltalks.shift(15),
           'current_page' => current_page,
           'num_pages' => total,
@@ -45,6 +53,57 @@ module Jekyll
         })
         current_page += 1
       end
+    end
+
+    def create_all_user_techtalks(site, alltalks)
+      layout = 'usertechtalks'
+      users_talks = Hash.new {|h,k| h[k] = []}
+      alltalks.each do |post|
+        users_talks[post.categories[1]] << post
+      end
+
+      users_talks.each do |user, talks|
+        user_name = user_to_name(user)
+        base = "/techtalks/#{user}/all/"
+        total = (talks.length / 15.to_f).ceil
+        first_page_posts = talks.shift(15)
+        site.pages << PagedTalk.new(site, site.source, base, 'index.html', layout, {
+          'posts'  => first_page_posts,
+          'current_page' => 1,
+          'num_pages' => total,
+          'title' => "#{user_name}'s Recent Techtalks",
+          'base' => base,
+          'user' => user
+        })
+        site.pages << PagedTalk.new(site, site.source, "#{base}page_1/", 'index.html', layout, {
+          'posts'  => first_page_posts,
+          'current_page' => 1,
+          'num_pages' => total,
+          'title' => "#{user_name}'s Recent Techtalks",
+          'base' => base,
+          'user' => user
+        })
+        current_page = 2
+        while talks.size > 0
+          site.pages << PagedTalk.new(site, site.source, "#{base}page_#{current_page}/", 'index.html', layout, {
+            'posts'  => talks.shift(15),
+            'current_page' => current_page,
+            'num_pages' => total,
+            'title' => "#{user_name}'s Archived Techtalks",
+            'base' => base,
+            'user' => user
+          })
+          current_page += 1
+        end
+      end
+    end
+
+    # Temporary reuse of code, this needs to be fixed
+    def user_to_name(author)
+      capitalize_all(author.gsub(/_/,' '))
+    end
+    def capitalize_all(author)
+      author.split(' ').collect{|x| x.capitalize}.join(' ')
     end
   end
 end
