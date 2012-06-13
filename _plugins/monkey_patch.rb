ab915e5a88731927cdba30e98514759f56134e1e
Will Brady <wbrady@nearinfinity.com>
2012-06-13 10:36:13 -0400

    Monkey patches

--- _plugins/monkey_patch.rb
+++ _plugins/monkey_patch.rb
@@ -11,7 +11,7 @@ module Jekyll
         self.content = Liquid::Template.parse(self.content).render!(payload, info)
       rescue => e
         puts "Liquid Exception: #{e.message} in #{self.data.inspect}"
-        abort("Failing Build")
+        #abort("Failing Build")
       end
 
       self.transform
@@ -30,7 +30,7 @@ module Jekyll
           self.output = Liquid::Template.parse(layout.content).render!(payload, info)
         rescue => e
           puts "Liquid Exception: #{e.message} in #{self.data.inspect}"
-          abort("Failing Build")
+          #abort("Failing Build")
         end
 
         if layout = layouts[layout.data["layout"]]
