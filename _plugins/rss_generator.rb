b8f91b491596f6bd6f25a05ee8b65855a3a085df
Will Brady <wbrady@nearinfinity.com>
2012-06-20 10:27:54 -0400

    Added tech talks, speaking engagements, and published works rss feeds

--- _plugins/rss_generator.rb
+++ _plugins/rss_generator.rb
@@ -49,7 +49,7 @@ module Jekyll
         selected_user << post
       end
 
-      create_blog_feeds(site, site.source, '/blogs/', {
+      create_feeds(site, '/blogs/', 'blogs.xml', {
         'posts'  => allblogs,
         'title' => "Blogs at Near Infinity",
         'link' => "http://www.nearinfinity.com/blogs",
@@ -59,7 +59,7 @@ module Jekyll
       # Generate the Authors Feeds
       sorted_blogs.each do |author, author_posts|
         formatted_author = author.split('_').collect{|x| x.capitalize}.join(' ')
-        create_blog_feeds(site, site.source, "/blogs/#{author}/rss/", {
+        create_feeds(site, "/blogs/#{author}/rss/", 'blogs.xml', {
           'posts'  => author_posts,
           'title' => "#{formatted_author} - Blogs at Near Infinity",
           'link' => "http://www.nearinfinity.com/blogs/#{author}",
@@ -70,7 +70,7 @@ module Jekyll
       # Generate the Tags Feeds
       site.tags.each do |tag, tag_posts|
         filtered_posts = tag_posts.reject{ |post| !post.categories.include? 'blogs' }.sort.reverse
-        create_blog_feeds(site, site.source, "/blogs/#{tag}/rss/", {
+        create_feeds(site, "/blogs/#{tag}/rss/", 'blogs.xml', {
           'posts'  => filtered_posts,
           'title' => "#{tag.capitalize} Related Blogs - Blogs at Near Infinity",
           'link' => "http://www.nearinfinity.com/tags/blogs/#{tag}",
@@ -79,38 +79,61 @@ module Jekyll
       end
       
       create_all_post_feeds(site)
-      
       create_news_feeds(site)
-    end
-
-    def create_blog_feeds(site, base, dir, data)
-      site.pages << RssFeed.new(site, base, dir, "index.xml", data)
-      site.pages << AtomFeed.new(site, base, dir, "atom.xml", data)
-      site.pages << ExcerptRssFeed.new(site, base, dir, "blogs.xml", data)
+      create_tech_talk_feeds(site)
+      create_speaking_engagement_feeds(site)
+      create_published_works_feeds(site)
     end
     
     def create_all_post_feeds(site)
-      data = {
+      create_feeds(site, '/', 'excerpt.xml', {
         'posts' => site.posts.sort.reverse,
         'title' => 'Near Infinity',
         'link' => 'http://www.nearinfinity.com',
-        'description' => 'Near Infinity Posts'
-      }
-      site.pages << RssFeed.new(site, site.source, '/', 'index.xml', data)
-      site.pages << AtomFeed.new(site, site.source, '/', 'atom.xml', data)
-      site.pages << ExcerptRssFeed.new(site, site.source, '/', 'excerpt.xml', data)
+        'description' => 'Near Infinity News, Blogs, Tech Talks, Published Works and Speaking Engagements'
+      })
     end
     
     def create_news_feeds(site)
-      data = {
-        'posts' => site.posts.reject{|post| post.categories[0] != 'news' }.sort.reverse,
+      create_feeds(site, '/news/', 'news.xml', {
+        'posts' => site.posts.reject{|post| !post.categories.include? 'news' }.sort.reverse,
         'title' => 'Near Infinity News',
         'link' => 'http://www.nearinfinity.com/news',
         'description' => 'Near Infinity News'
-      }
-      site.pages << RssFeed.new(site, site.source, '/news/', 'index.xml', data)
-      site.pages << AtomFeed.new(site, site.source, '/news/', 'atom.xml', data)
-      site.pages << ExcerptRssFeed.new(site, site.source, '/news/', 'excerpt.xml', data)
+      })
+    end
+    
+    def create_tech_talk_feeds(site)
+      create_feeds(site, '/techtalks/', 'tech_talks.xml', {
+        'posts' => site.posts.reject{|post| !post.categories.include? 'techtalks' }.sort.reverse,
+        'title' => 'Near Infinity Tech Talks',
+        'link' => 'http://www.nearinfinity.com/techtalks',
+        'description' => 'Near Infinity Tech Talks'
+      })
+    end
+    
+    def create_speaking_engagement_feeds(site)
+      create_feeds(site, '/speaking/', 'speaking_engagements.xml', {
+        'posts' => site.posts.reject{|post| !post.categories.include? 'speaking' }.sort.reverse,
+        'title' => 'Near Infinity Speaking Engagements',
+        'link' => 'http://www.nearinfinity.com/speaking',
+        'description' => 'Near Infinity Speaking Engagements'
+      })
+    end
+    
+    def create_published_works_feeds(site)
+      create_feeds(site, '/published_works/', 'published_works.xml', {
+        'posts' => site.posts.reject{|post| !post.categories.include? 'published_works' }.sort.reverse,
+        'title' => 'Near Infinity Published Works',
+        'link' => 'http://www.nearinfinity.com/published_works',
+        'description' => 'Near Infinity Published Works'
+      })
+    end
+    
+    def create_feeds(site, base, excerpt_file_name, data)
+      site.pages << RssFeed.new(site, site.source, base, 'index.xml', data)
+      site.pages << AtomFeed.new(site, site.source, base, 'atom.xml', data)
+      site.pages << ExcerptRssFeed.new(site, site.source, base, excerpt_file_name, data)
     end
   end
 end
