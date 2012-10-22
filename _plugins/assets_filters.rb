32db517d0e909dc0fbae68c3f897a5ec42adc1b8
Will Brady <wbrady@nearinfinity.com>
2012-10-22 17:45:48 -0400

    Now pull simages from Dave's images

--- _plugins/assets_filters.rb
+++ _plugins/assets_filters.rb
@@ -3,7 +3,7 @@ module Jekyll
     # Returns a users image url
     def user_image(site, user, class_name="")
       if employee_number = site['pages'].select{|page| page.instance_variable_get('@dir').end_with? user}.first.data['employee_number']
-        "<img class='#{class_name}' src='/assets/images/users/#{employee_number}.png' username='#{user}' />"
+        "<img class='#{class_name}' src='http://www.nearinfinity.com/images/vector_art_circle/#{employee_number}.png' username='#{user}' />"
       else
         puts "User image not found for #{user}"
         ""
