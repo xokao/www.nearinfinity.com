7924c800bea6664c4442a45101df8ac567252a7f
Will Brady <wbrady@nearinfinity.com>
2012-10-25 15:01:45 -0400

    Added employee_map.json generation for NIC+

--- _plugins/bio_page_generator.rb
+++ _plugins/bio_page_generator.rb
@@ -11,16 +11,39 @@ module Jekyll
     end
   end
 
+  class EmployeeMapPage < Page
+    def initialize(site, base, dir, name, data)
+      self.data = data.clone
+      self.data['layout'] = 'employee_map'
+      super(site, base, dir, name)
+    end
+
+    def read_yaml(_, __)
+      # Do nothing, Allows you to make pages that dont have pages at the location they are built in
+    end
+  end
+
   class BioGenerator < Generator
     safe true
 
     def generate(site)
+      employees = []
       site.pages.each do |page|
         directory_parse = page.instance_variable_get('@dir').split('/')
         next if !bio_sub_directory? directory_parse
         page.data.merge!({'layout' => 'person', 'user' => directory_parse.last})
+        employees << page
         site.pages << ShortBioPage.new(site, site.source, '/short_bios/popup_bio/', "#{page.data['user']}.html", page.data)
       end
+
+      data = {
+        "employees" => employees.map{|page| {
+          'employee_number' => page.data['employee_number'],
+          'blog_name' => page.data['user']
+        }}
+      }
+
+      site.pages << EmployeeMapPage.new(site, site.source, '/', "employee_map.json", data)
     end
 
     private
